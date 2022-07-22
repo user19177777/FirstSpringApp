5810357378ba70961fdad375e78c3bcddcb41813
Ivan Ivanov <79523701529@yandex.ru>
2022-07-22 15:56:25 +0300

    add appContext.xml(there will be determinate beans) beans definition in appContext.xml dependency injection via constructor

--- src/main/webapp/java/TestApp.java
+++ src/main/webapp/java/TestApp.java
@@ -1,7 +1,15 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestApp {
-    public static void main(String[] args) {
-
+        public static void main(String[] args) {
+            //инициализируем контекст
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                    "appContext.xml"
+            );
+            //достаем бин из контекста
+            Music music = context.getBean("music",ClassicMusic.class);
+            MusicPlayer player = context.getBean("musicPlayer",MusicPlayer.class);
+            player.play();
+            context.close();
     }
 }
