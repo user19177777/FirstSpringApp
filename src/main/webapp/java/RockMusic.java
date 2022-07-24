44dbbde9a4f53fc71440aaedebfb6a20699c2840
Ivan Ivanov <79523701529@yandex.ru>
2022-07-24 17:42:33 +0300

    init-method, destroy-method

--- src/main/webapp/java/RockMusic.java
+++ src/main/webapp/java/RockMusic.java
@@ -3,4 +3,10 @@ public class RockMusic implements Music{
     public String getSong() {
         return "Rock_music";
     }
+    public void doMyInit(){
+        System.out.println("Init_method");
+    }
+    public void doMyDestroy(){
+        System.out.println("Destroy_method");
+    }
 }
