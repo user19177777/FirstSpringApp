adb4f296ff7364911e9c3390cb29a4f0542ee374
Ivan Ivanov <79523701529@yandex.ru>
2022-07-24 17:30:20 +0300

    scope singleton

--- src/main/webapp/java/TestApp.java
+++ src/main/webapp/java/TestApp.java
@@ -9,6 +9,14 @@ public class TestApp {
             //достаем бин из контекста
             MusicPlayer player = context.getBean("musicPlayer",MusicPlayer.class);
             player.play();
+
+            //создает один объект и потом использует его ссылку
+            Music music1 = context.getBean("rockMusic",RockMusic.class);
+            Music music2 = context.getBean("rockMusic",RockMusic.class);
+            System.out.println(music1 == music2);
+            System.out.println(music1.toString());
+            System.out.println(music2.toString());
+
             context.close();
     }
 }
