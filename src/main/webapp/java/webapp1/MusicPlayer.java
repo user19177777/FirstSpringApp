39c90c17839a439e547f8b6878a98fa164bc3c25
Ivan Ivanov <79523701529@yandex.ru>
2022-07-24 19:11:49 +0300

    @Autowired via constructor

--- src/main/webapp/java/webapp1/MusicPlayer.java
+++ src/main/webapp/java/webapp1/MusicPlayer.java
@@ -1,11 +1,14 @@
 package webapp1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
     Music music;
     // через конструктор
+    //автоматически подбирает бин(автосвязывание)
+    @Autowired
     public MusicPlayer(Music music) {
         this.music = music;
     }
--- src/main/webapp/java/webapp1/TestApp.java
+++ src/main/webapp/java/webapp1/TestApp.java
@@ -6,8 +6,6 @@ public class TestApp {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
-
-        Music music = context.getBean("classicMusic",ClassicMusic.class);
         MusicPlayer player = context.getBean("musicPlayer",MusicPlayer.class);
         player.play();
         context.close();
