dcc82a97c690da943ce2e55541beef5e04fb4816
Ivan Ivanov <79523701529@yandex.ru>
2022-07-24 17:49:57 +0300

    init-method, destroy-method

--- src/main/webapp/java/TestApp.java
+++ src/main/webapp/java/TestApp.java
@@ -10,7 +10,7 @@ public class TestApp {
             MusicPlayer player = context.getBean("musicPlayer",MusicPlayer.class);
             player.play();
 
-            //каждый раз при вызове getBean() создает новый объект
+
             Music music1 = context.getBean("rockMusic",RockMusic.class);
             Music music2 = context.getBean("rockMusic",RockMusic.class);
             System.out.println(music1 == music2);
