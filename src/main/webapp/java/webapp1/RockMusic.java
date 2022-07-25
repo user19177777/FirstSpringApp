23c5dbc3b48654a36034e5f5c0e568d3c2814eff
Ivan Ivanov <79523701529@yandex.ru>
2022-07-25 12:50:01 +0300

    init-method, destroy-method @PostConstruct @PreDestroy

--- src/main/webapp/java/webapp1/RockMusic.java
+++ src/main/webapp/java/webapp1/RockMusic.java
@@ -3,12 +3,21 @@ package webapp1;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-//prototype
-@Scope("prototype")
 public class RockMusic implements Music{
     @Override
     public String getSong() {
         return "Rock_music";
+    }
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Init_method");
+    }
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Destroy_method");
     }
 }
--- src/main/webapp/java/webapp1/TestApp.java
+++ src/main/webapp/java/webapp1/TestApp.java
@@ -8,13 +8,7 @@ public class TestApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
         MusicPlayer player = context.getBean("musicPlayer",MusicPlayer.class);
         player.play();
-        System.out.println(player.getVolume());
-        //создает один объект и потом использует его ссылку
-        Music music1 = context.getBean("rockMusic",RockMusic.class);
-        Music music2 = context.getBean("rockMusic",RockMusic.class);
-        System.out.println(music1 == music2);
-        System.out.println(music1.toString());
-        System.out.println(music2.toString());
+
         context.close();
 
     }
